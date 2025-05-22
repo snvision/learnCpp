20979bd8e12e5e2c911e117671c9bcfbfa7121f3
snvision <savchienkov.nikita@mail.ru>
2025-05-22 20:50:43 +0300

    review

--- projects/c1s1_ui/src/MainWindow.cpp
+++ projects/c1s1_ui/src/MainWindow.cpp
@@ -8,7 +8,7 @@ MainWindow::~MainWindow() = default;
 
 MainWindow::MainWindow(QWidget* parent)
   : QMainWindow{parent}
-    , ui{new Ui::MainWindow} {
+, ui{new Ui::MainWindow} {
   ui->setupUi(this);
 }
 
@@ -17,33 +17,28 @@ void MainWindow::on_pb_main_released() const {
   ui->l_main_answer->setText("main()");
 }
 
-
 void MainWindow::on_pb_statement_released() const {
-  ui->l_statement_answer->setText("Statement — sequence of instruction in computer program, ended ;");
+  ui->l_statement_answer->setText("Statement — type of instruction in computer program, "
+                                    "that telling computer to perform an action.");
 }
-
 
 void MainWindow::on_pb_function_released() const {
-  ui->l_function_answer->setText("Function — a named sequence of code block, "
-    "that can be reused several times.");
+  ui->l_function_answer->setText("Function — a named sequence of statements, "
+                                   "that can be reused several times.");
 }
-
 
 void MainWindow::on_pb_run_released() const {
-  ui->l_run_answer->setText("ОС инициализирует программу и передаёт управление функции main");
+  ui->l_run_answer->setText("The statements in main() are executed in sequence order.");
 }
-
 
 void MainWindow::on_pb_symbol_released() const {
   ui->l_symbol_answer->setText(";");
 }
 
-
 void MainWindow::on_pb_error_released() const {
   ui->l_error_answer->setText("Syntax error — error in using rules of the C++ language.");
 }
 
-
 void MainWindow::on_pb_library_released() const {
-  ui->l_library_answer->setText("Набор функций, методов и классов, которые можно использовать для написания программы и обеспечивает связь программы с ОС");
+  ui->l_library_answer->setText("Library — code for subsequence reuse.");
 }
